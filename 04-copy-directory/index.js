07a0fe985d52a60ed156533bfcda7093869c61f3
OAK <oak110375@gmail.com>
2022-05-21 23:31:08 +0300

    task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.rm(path.join(__dirname, 'files-copy'), { force: true, recursive: true }, (err) => {
+  if (err) throw err;
+  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+    if (err) throw err;
+    fs.readdir(path.join(__dirname, 'files'), { withFileTypes: true }, (err, items) => {
+      if (err) throw err;
+      items.forEach((i) => {
+        fs.copyFile(path.join(__dirname, 'files', i.name), path.join(__dirname, 'files-copy', i.name), (err) => {
+          if (err) throw err;
+        });
+      });
+    });
+  });
+});
