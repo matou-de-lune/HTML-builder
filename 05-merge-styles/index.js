aff6c417ddf1b5dc63666868b384842becbba0f8
OAK <oak110375@gmail.com>
2022-05-22 00:06:48 +0300

    task 05

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const output = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'));
+
+fs.readdir(path.join(__dirname, 'styles'), { withFileTypes: true }, (err, items) => {
+  if (err) throw err;
+  items.forEach((i) => {
+    if (i.isFile() && (path.extname(i.name).slice(1) === 'css')) {
+      fs.readFile(path.join(__dirname, 'styles', i.name), (err, data) => {
+        if (err) throw err;
+        output.write(data);
+      });
+    }
+  });
+});
