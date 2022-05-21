6669388d544441bfe1d0b63ed17c56bc8d7db359
OAK <oak110375@gmail.com>
2022-05-21 22:49:43 +0300

    task 03

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,9 +2,11 @@ const fs = require('fs');
 const path = require('path');
 
 fs.readdir(path.join(__dirname, 'secret-folder'), { withFileTypes: true }, (err, items) => {
+  if (err) throw err;
   items.forEach((i) => {
     if (i.isFile()) {
       fs.stat(path.join(__dirname, 'secret-folder', i.name), (e, s) => {
+        if (e) throw e;
         console.log(`${i.name.slice(0, i.name.lastIndexOf(path.extname(i.name)))} - ${path.extname(i.name).slice(1)} - ${s.size}`);
       });
     }
